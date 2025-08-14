828afc54cfe7c5361f8d2f51b551229249b5f76d
Lua <lala.trindade.2008@gmail.com>
2025-08-13 21:20:31 -0300

    wip

--- app/main.py
+++ app/main.py
@@ -1,4 +1,5 @@
 ﻿from fastapi import FastAPI
+from fastapi.middleware.cors import CORSMiddleware
 
 from app.core.configs import settings
 from app.api.api import api_router
@@ -8,6 +9,20 @@ app = FastAPI(title="TCC API")
 
 app.include_router(api_router, prefix=settings.API_STR)
 
+origins = [
+    "http://localhost:4200",  # Angular local
+    "http://127.0.0.1:4200",  # alternativa local
+    "https://seu-frontend.com.br",  # (caso tenha um ambiente de produção)
+]
+
+# Middleware de CORS
+app.add_middleware(
+    CORSMiddleware,
+    allow_origins=origins,  # ou ["*"] para todos os domínios (não recomendado em produção)
+    allow_credentials=True,
+    allow_methods=["*"],  # permite todos os métodos (GET, POST, OPTIONS, etc.)
+    allow_headers=["*"],  # permite todos os headers (incluindo Authorization)
+)
 
 # @app.get("health-check")
 # def health_check():
